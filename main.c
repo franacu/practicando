60e1ebae35ca56d5e784ecd22d3a2eccf7363d0f
francisco <fmacuna@fie.undef.edu.ar>
2023-03-29 22:18:49 -0300

    inicializando

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    int nota,i=0,t=0,cont=0,prom;
+    do
+    {
+        printf("Ingrese la nota del alumno o 0 para finalizar:");
+        scanf("%d",&nota);
+    }while(nota>10 || nota<=0);
+    while(nota!=0)
+    {
+        cont++;
+        t=t+nota;
+        prom=t/cont;
+        printf("Ingrese la nota del alumno o 0 para finalizar:");
+        scanf("%d",&nota);
+    }
+    printf("El promedio es de %d",prom);
+
     return 0;
 }
